2f62cf85f2d40dc38cf33b28580669ac9aba6d5c
Luka Krivacevic <krivacevicluka@gmail.com>
2019-11-20 10:52:41 +0100

    End of section 10

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -40,6 +42,23 @@ namespace DatingApp.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto) 
+        {
+            //check if user id matches id from user's token - so that other user can not make change to different user
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);  //maps and rewrites userForUpdateDto to userFromRepo
+
+            if(await _repo.SaveAll())
+                return NoContent(); //ako je uspesno sacuvano, vraca se NoContent
+
+            throw new Exception($"Updating user {id} failed on save");                
         }
     }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>(); //from UserForUpdateDto (from angular) to User for database
         }
     }
 }
